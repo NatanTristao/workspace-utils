eca44411f67f2682a792f1d3f35b42b1dace99c6
Natan Tristão <62655470+NatanTristao@users.noreply.github.com>
2025-07-17 15:39:14 -0300

    Add new options

--- app.py
+++ app.py
@@ -16,7 +16,32 @@ app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
 
 @app.route('/')
 def index():
-    return render_template('index.html', image_generated=False)
+    return render_template('index.html')
+
+@app.route('/defesa_facil')
+def defesa_facil():
+    return render_template('defesa_facil.html', image_generated=False)
+
+@app.route('/seminar_art_generator')
+def seminar_art_generator():
+    return render_template('seminar_art_generator.html', image_generated=False)
+
+@app.route('/drive')
+def go_to_drive():
+    return redirect('https://drive.google.com/drive/u/0/my-drive', code=302)
+
+@app.route('/canva')
+def go_to_canva():
+    return redirect('https://www.canva.com/', code=302)
+
+@app.route('/senhas')
+def passwords():
+    return redirect('https://drive.google.com/file/d/11oreuaEYvus529albJyRa7Y_ToLcCRP_/view?usp=sharing', code=302)
+
+@app.route('/totem')
+def totem_visualization():
+    return redirect('https://www.inf.ufrgs.br/totem/?preview=true&showImages=true', code=302)
+
 
 @app.route('/generate', methods=['POST'])
 def generate():
@@ -43,6 +68,28 @@ def generate():
 
     flash('Imagem gerada com sucesso!')
     return render_template('index.html', image_generated=True, image_url=url_for('static', filename='outputs/seminar_art.png'))
+
+@app.route('/generate_defesa', methods=['POST'])
+def generate_defesa(): 
+    data = {
+        "degree_type":    request.form['degree_type'],   
+        "course":         request.form['course'],         
+        "candidate_name": request.form['candidate_name'], 
+        "advisor":        request.form['advisor'],        
+        "coadvisor":      request.form.get('coadvisor', ''),   
+        "title":          request.form['title'],         
+        "date":           request.form['date'],           
+        "time":           request.form['time'],          
+        "stream_link":    request.form.get('stream_link', ''), 
+        "observation":    request.form.get('observation', ''), 
+    }
+
+    flash('Arte de defesa gerada com sucesso!')
+    return render_template(
+        'defesa_facil.html',
+        image_generated=True,
+        image_url=url_for('static', filename=f'outputs/{output_filename}') 
+        )
 
 if __name__ == '__main__':
     app.run(debug=True)
